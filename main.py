1427bb689f549862b278db899ce810fe0de3845e
Colin <colin.h.ayres@gmail.com>
2024-12-27 21:25:18 +0100

    Add graph drawing

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def main():
+    # Create a random graph
+    G = nx.erdos_renyi_graph(n=10, p=0.15)  # 100 nodes, 5% connection probability
+    nx.draw(G, with_labels=True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
