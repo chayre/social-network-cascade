468d607681b355d9dfcab49a42ac8787d9476c16
Colin <colin.h.ayres@gmail.com>
2024-12-28 17:07:51 +0100

    Adding plotting function
    Plots the graph of nodes (active nodes and seed nodes are not needed)

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,1 +1,20 @@
-#Plotting here
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def plot_graph(G, pos, active_nodes=[], seed_nodes=[]):
+    """
+    Plots the graph with nodes color-coded based on their status.
+    
+    Parameters:
+    - G: The graph
+    - pos: Positions of nodes 
+    - active_nodes: Set of nodes that are active
+    - seed_nodes: List of initial seed nodes
+    """
+    colors = [
+        'red' if node in active_nodes else 'green' if node in seed_nodes else 'blue'
+        for node in G.nodes()
+    ]
+    nx.draw(G, pos, with_labels=True, node_color=colors, node_size=300)
+    plt.title("Graph Visualization with Active and Seed Nodes")
+    plt.show()
