bdfafb1bfde31ad4362ffda120900ef59c7684ac
Colin <colin.h.ayres@gmail.com>
2025-01-06 23:25:32 +0100

    Added import graph util

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils.cascade_simulation import create_graph, independent_cascade, find_influencers
 from utils.linear_threshold_simulation import linear_threshold_model, apply_weights
-from utils.plotting import plot_graph, compute_positions
+from utils.plotting import plot_graph, compute_positions, import_graph
 from utils.analysis import detect_communities, centrality_analysis, evolve_graph
 
 def main():
